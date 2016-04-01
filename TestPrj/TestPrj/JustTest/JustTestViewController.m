7f2b74854abe650e69b88be1c6ea2671c52759c5
F－03 <291194609@qq.com>
2016-04-01 14:33:02 +0800

    JustTest

---
+++ TestPrj/TestPrj/JustTest/JustTestViewController.m
@@ -0,0 +1,38 @@
+//
+//  JustTestViewController.m
+//  TestPrj
+//
+//  Created by F－03 on 16/4/1.
+//  Copyright © 2016年 hjs. All rights reserved.
+//
+
+#import "JustTestViewController.h"
+
+@interface JustTestViewController ()
+
+@end
+
+@implementation JustTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"JustTest");
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
